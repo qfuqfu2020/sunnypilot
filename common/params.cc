53cacd5a6f1c151a5fb5263dd1b7d2bd90570fa1
rav4kumar <meetkumardesai@gmail.com>
2025-01-20 06:53:40 -0700

    eco and tune

--- common/params.cc
+++ common/params.cc
@@ -231,6 +231,9 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"HyundaiRadarTracksToggle", PERSISTENT},
 
     {"DynamicExperimentalControl", PERSISTENT},
+    {"ToyotaAutoHold", PERSISTENT},
+    {"ToyotaEnhancedBsm", PERSISTENT},
+    {"ToyotaTSS2Long", PERSISTENT},
 };
 
 } // namespace
